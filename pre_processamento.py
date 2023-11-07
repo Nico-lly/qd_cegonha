f8cd12712ab7e3a90948769337cfa832fdca0792
Luísa F. Coelho <fancelli_luisa@hotmail.com>
2023-11-07 11:57:33 -0300

    reading files

--- pre_processamento.py
+++ pre_processamento.py
@@ -3,9 +3,15 @@ import pathlib
 
 PATH = (".")
 
-diarios = pd.read_csv("./raw_data/saude_dataset1.csv", header = 1, encoding='utf-8')
+diarios = pd.read_csv("./raw_data/saude_dataset1.csv", header = 0, encoding='utf-8')
 print(diarios.head(10))
 
 print(list(diarios.columns))
-diarios_dataset = diarios["excerpt", "excerpt_subthemes"]
-#print(diarios_dataset.head(5))
+diarios_dataset = diarios[['excerpt', 'excerpt_subthemes']]
+print(diarios_dataset.head(5))
+
+### Tecnicas de Pré-processamento
+# Stopwords
+
+# Remover pontuação
+
