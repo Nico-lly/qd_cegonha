b209d1435837aabe6fb7a605f9ac3d2169f1ae16
Luísa F. Coelho <fancelli_luisa@hotmail.com>
2023-11-09 15:32:27 -0300

    add shp to gitignore

--- maps.py
+++ maps.py
@@ -13,7 +13,7 @@ import re
 PATH = (".")
 
 map_meso = gpd.read_file('./mapas/mesoregiao-ipeageo/mesoregiao.shp')
-map_mun = gpd.read_file('./mapas/BR_Municipios_2022/BR_Municipios_2022.shp')
+map_mun = gpd.read_file('./mapas/BR_Municipios_2022/BR_Municipios_2022.shp')  #from: https://www.ibge.gov.br/geociencias/organizacao-do-territorio/malhas-territoriais/15774-malhas.html
 print(map_mun.head())
 
 #sns.set(style="white", palette="deep", color_codes=True)
